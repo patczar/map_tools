1d6fdfeb6e538ee39f0d89b2d6fbba3a9b7d430a
Patryk Czarnik <patryk@patrykczarnik.net>
2017-12-30 17:47:16 +0100

    Basic exception class

---
+++ map_tools/src/main/java/net/patrykczarnik/map_tools/exn/MapToolsException.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package net.patrykczarnik.map_tools.exn;
+
+/**
+ * @author Patryk Czarnik <patryk@patrykczarnik.net>
+ * 
+ * The base class for all custom exceptions.
+ */
+public class MapToolsException extends Exception {
+	public MapToolsException() {
+	}
+
+	public MapToolsException(String message) {
+		super(message);
+	}
+
+	public MapToolsException(Throwable cause) {
+		super(cause);
+	}
+
+	public MapToolsException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
