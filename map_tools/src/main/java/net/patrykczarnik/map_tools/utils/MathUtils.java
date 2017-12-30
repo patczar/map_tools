fc4092172551c77a9b81b9504c9b15848dab3da7
Patryk Czarnik <patryk@patrykczarnik.net>
2017-12-30 19:35:10 +0100

    A class for math utilities.

---
+++ map_tools/src/main/java/net/patrykczarnik/map_tools/utils/MathUtils.java
@@ -0,0 +1,23 @@
+package net.patrykczarnik.map_tools.utils;
+
+/**
+ * @author Patryk Czarnik <patryk@patrykczarnik.net>
+ *
+ * A utility class with mathematical operations.
+ */
+public final class MathUtils {
+
+	/** This class is not intended to be instantiated. */
+	private MathUtils() {
+	}
+	
+	/** Chcecks whether the argument fits between the two given numbers.
+	 * If yes, nothing happens.
+	 * If no, then an IllegalArgumentException is thrown.
+	 */
+	public static void checkArgBetween(double aArgument, double aMin, double aMax, String aMessage) throws IllegalArgumentException {
+		if(aArgument < aMin || aArgument > aMax) {
+			throw new IllegalArgumentException(aMessage);
+		}
+	}
+}
