897eb6d3176282d2b9bcea4fef9748fad112d627
Patryk Czarnik <mail@patrykczarnik.net>
2018-01-18 21:40:19 +0100

    OSMPoint.of01Point tests and a bugfix.

--- map_tools/src/main/java/net/patrykczarnik/map_tools/osm/OSMConstants.java
+++ map_tools/src/main/java/net/patrykczarnik/map_tools/osm/OSMConstants.java
@@ -14,7 +14,7 @@ public final class OSMConstants {
 	static final int PIXELS_OF_WORLD_BITS = 31;
 
 	/** The number of bits for pixel coordinates in the maximum supported scale. */
-	static final long PIXELS_OF_WORLD = 1 << PIXELS_OF_WORLD_BITS;
+	static final long PIXELS_OF_WORLD = 1L << PIXELS_OF_WORLD_BITS;
 
 	/** The number of bits which store the pixel coordinate within a tile. */
 	static final int PIXELS_OF_TILE_BITS = 8;
---
+++ map_tools/src/test/java/net/patrykczarnik/map_tools/osm/OSMPoint01_Test.java
@@ -0,0 +1,53 @@
+package net.patrykczarnik.map_tools.osm;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+import net.patrykczarnik.map_tools.graphic.PixelCoordinates;
+
+public class OSMPoint01_Test {
+
+	@Test
+	public void testof01_0() {
+		final OSMPoint point = OSMPoint.of01Point(0.0, 0.0);		
+		final OSMTile tile0 = point.getTile(0);
+		final PixelCoordinates px0 = point.getCoordinatesWithinTile(0);
+		assertThat(tile0.x).isEqualTo(0);
+		assertThat(tile0.y).isEqualTo(0);
+		assertThat(px0.x).isEqualTo(0);
+		assertThat(px0.y).isEqualTo(0);
+
+		final OSMTile tile3 = point.getTile(3);
+		final PixelCoordinates px3 = point.getCoordinatesWithinTile(3);
+		assertThat(tile3.x).isEqualTo(0);
+		assertThat(tile3.y).isEqualTo(0);
+		assertThat(px3.x).isEqualTo(0);
+		assertThat(px3.y).isEqualTo(0);
+	}
+
+	@Test
+	public void testof01_05() {
+		final OSMPoint point = OSMPoint.of01Point(0.5, 0.25);		
+		final OSMTile tile0 = point.getTile(0);
+		final PixelCoordinates px0 = point.getCoordinatesWithinTile(0);
+		assertThat(tile0.x).isEqualTo(0);
+		assertThat(tile0.y).isEqualTo(0);
+		assertThat(px0.x).isEqualTo(128);
+		assertThat(px0.y).isEqualTo(64);
+
+		final OSMTile tile1 = point.getTile(1);
+		final PixelCoordinates px1 = point.getCoordinatesWithinTile(1);
+		assertThat(tile1.x).isEqualTo(1);
+		assertThat(tile1.y).isEqualTo(0);
+		assertThat(px1.x).isEqualTo(0);
+		assertThat(px1.y).isEqualTo(128);
+
+		final OSMTile tile2 = point.getTile(2);
+		final PixelCoordinates px2 = point.getCoordinatesWithinTile(2);
+		assertThat(tile2.x).isEqualTo(2);
+		assertThat(tile2.y).isEqualTo(1);
+		assertThat(px2.x).isEqualTo(0);
+		assertThat(px2.y).isEqualTo(0);
+	}
+}
