9cda405aa414f4c22a6e84266cb3167dc1270732
Patryk Czarnik <mail@patrykczarnik.net>
2018-01-28 21:05:57 +0100

    Complete convertions between geographical coordinates and pixel coordinates.

---
+++ map_tools/src/main/java/net/patrykczarnik/map_tools/osm/Convertions.java
@@ -0,0 +1,45 @@
+package net.patrykczarnik.map_tools.osm;
+
+import java.util.stream.Stream;
+
+import net.patrykczarnik.map_tools.geo.GeographicalCoordinates;
+import net.patrykczarnik.map_tools.geo.GeographicalPath;
+import net.patrykczarnik.map_tools.graphic.PixelCoordinates;
+import net.patrykczarnik.map_tools.graphic.PixelPath;
+
+/**
+ * @author Patryk Czarnik <patryk@patrykczarnik.net>
+ *
+ * Utility class implementing convertions between various types of points/paths.
+ * In particular convertions which are indirect or from other reasons should not be defined individually for particular objects.
+ */
+public final class Convertions {
+	/** Converts points specified with geographical coordinates into coordinates of pixels in a whole world map in the given scale. Stream version.
+	 * @param aStream the source of points
+	 * @param aScale the scale of the target map
+	 * @return
+	 */
+	public static Stream<PixelCoordinates> geographicalCoordinatesToPixelPointsStream(
+			final Stream<GeographicalCoordinates> aStream,
+			final int aScale) {
+		return aStream
+			.map(MercatorProjection::projectPoint01)
+			.map(OSMPoint::ofCenteredPoint)
+			.map(p -> p.getAbsoluteCoordinates(aScale));
+	}
+
+	/** Converts points specified with geographical coordinates into coordinates of pixels in a whole world map in the given scale. Complete objects version.
+	 * @param aStream the source of points
+	 * @param aScale the scale of the target map
+	 * @return
+	 */
+	public static PixelPath geographicalPathToPixelPath(
+			final GeographicalPath aGeoPath,
+			final int aScale) {
+		Stream<PixelCoordinates> pixelStream = aGeoPath.stream()
+			.map(MercatorProjection::projectPoint01)
+			.map(OSMPoint::ofCenteredPoint)
+			.map(p -> p.getAbsoluteCoordinates(aScale));
+		return PixelPath.of(pixelStream);
+	}
+}
